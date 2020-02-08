7fe40039f01570ea4106b3c5a238986ab1d56419
alleytseng <fungelar@gmail.com>
2020-02-08 15:23:11 +0800

    Modify syntsx error

--- demo0403.php
+++ demo0403.php
@@ -1,10 +1,10 @@
 <HTML>
 <HEAD><TITLE>第一個互動程式</TITLE></HEAD>
-<body>
-	<form action="demo0401.php" method="get">
+<BODY>
+	<FORM action="demo0401.php" method="get">
 		請輸入姓名:<input type="text" name="somebody">
 		稱謂:<input type="text" name="title">	
 		<input type="submit" name="送出">		
-	</form>
-</body>	
+	</FORM>
+</BODY>	
 </HTML>
