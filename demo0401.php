8a8e7916b12b806fe5c273fff41fa5e66b473d37
alleytseng <fungelar@gmail.com>
2020-02-08 15:11:35 +0800

    HTML:form

---
+++ demo0401.php
@@ -0,0 +1,10 @@
+<HTML>
+<HEAD> my first php program </HEAD>
+<BODY>
+<?php
+$my_name = "Alley";
+echo $_GET['somebody'].$_GET['title'].", hello, welocome to {$my_name}'s php program!";
+//phpinfo();
+?>
+</BODY>
+</HTML>
