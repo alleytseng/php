9c0b2fc842ca91715d79e8d4e191ad79e564b209
alleytseng <fungelar@gmail.com>
2020-02-09 16:06:22 +0800

    show week when input date

---
+++ demo0802_input.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+	<form action="demo0802_output.php" methord="get">
+		請輸入日期yyyy-mm-dd:<input type="text" name="someday">
+		<input type="submit" name="送出">
+	</form>
+</body>
+</html>
---
+++ demo0802_output.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+	<?php
+	$aDay = $_GET['someday'];
+	$d=explode("-",$aDay);
+	/* 將字串藉由delimiter分離, 回傳陣列: explode ( string $delimiter , string $string [, int $limit = PHP_INT_MAX ] ) : array*/
+	$week=date("l",mktime(0,0,0,$d[1],$d[2],$d[0]));
+	if(!checkdate($d[1], $d[2], $d[0]))
+	/* 實驗發現格式正確會傳1, 但是格式錯誤不會傳0. checkdate ( int $month , int $day , int $year ) : bool*/
+		echo "輸入的日期不對 <br>!!!";
+	echo "$d[0] $d[1] $d[2] is $week";
+	?>
+</body>
+</html>
