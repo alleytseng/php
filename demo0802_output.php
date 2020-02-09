ffd4ebfbb7a50b28aa86337b7b83ea68c3451af2
alleytseng <fungelar@gmail.com>
2020-02-09 16:12:50 +0800

    show week only when input date format is correct.

--- demo0802_output.php
+++ demo0802_output.php
@@ -8,7 +8,8 @@
 	if(!checkdate($d[1], $d[2], $d[0]))
 	/* 實驗發現格式正確會傳1, 但是格式錯誤不會傳0. checkdate ( int $month , int $day , int $year ) : bool*/
 		echo "輸入的日期不對 <br>!!!";
-	echo "$d[0] $d[1] $d[2] is $week";
+	else
+		echo "$d[0] $d[1] $d[2] is $week";
 	?>
 </body>
 </html>
